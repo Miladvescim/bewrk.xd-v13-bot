f0dd17f2b99dbf23feab5ac3d8fe6404662b43e6
Miladvescim <miladvesv@gmail.com>
2025-01-29 00:06:25 +0300

    Add files via upload

---
+++ Global/Plugins/Telegram/_index.js
@@ -0,0 +1,16 @@
+
+const TBOT = require('node-telegram-bot-api');
+let chatIds = [""]
+class Telegram extends TBOT {
+    constructor() {
+        super("", {polling: true})
+    }
+
+    async bilgilendirmeGönder(content) {
+        chatIds.map(id => {
+            this.sendMessage(id, `${content}`)
+        })
+    }
+}
+
+module.exports = {Telegram}
